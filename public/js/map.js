bb0cec3585737861fd90fbf755a1b52bd746f095
SpyCop <g.c.vandenbrink@student.utwente.nl>
2022-07-08 12:08:14 +0200

    add polygon creation buttons

--- public/js/map.js
+++ public/js/map.js
@@ -1,6 +1,6 @@
 /*
  * Javascript for the Leaflet map
- * 6 July 2021, SpyCop
+ * 7 July 2021, SpyCop
  */
 
 var map = L.map('map').setView([52.045, 5.64], 17); // center map on home
@@ -10,3 +10,18 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '© <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+// add Leaflet-Geoman controls with some options to the map
+map.pm.addControls({
+  position: 'topleft',
+  drawMarker: false,
+  drawCircleMarker: false,
+  drawPolyline: false,
+  drawRectangle: false,
+  drawCircle: false,
+  drawText: false,
+  cutPolygon: false,
+  rotateMode: false,
+  // remove following line when implementing editing
+  editControls: false
+});
